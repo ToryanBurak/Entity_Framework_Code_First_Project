3d9a4dbbcf7f5aea82c94dc4c7316772140f9030
ToryanBurak <buraktoryan@gmail.com>
2023-01-18 12:32:47 +0300

    Create DbTool.cs

---
+++ 100123_DbCodeFirst_Proje/Design Patterns/Singleton Pattern/DbTool.cs
@@ -0,0 +1,27 @@
+﻿using _100123_DbCodeFirst_Proje.Models.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100123_DbCodeFirst_Proje.Design_Patterns.Singleton_Pattern
+{
+    using Models.Context;
+    public class DbTool
+    {
+        DbTool() { }
+        static MyContext dbInstance;
+        public static MyContext DBInstance
+        {
+            get
+            {
+                if (dbInstance == null)
+                {
+                    dbInstance = new MyContext();
+                }
+                return dbInstance;
+            }
+        }
+    }
+}
