e2481de34194eee0737287495850d5ff43cf9459
ToryanBurak <buraktoryan@gmail.com>
2023-01-18 12:32:57 +0300

    Create Configuration.cs

---
+++ 100123_DbCodeFirst_Proje/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace _100123_DbCodeFirst_Proje.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<_100123_DbCodeFirst_Proje.Models.Context.MyContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(_100123_DbCodeFirst_Proje.Models.Context.MyContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
